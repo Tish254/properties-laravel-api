101b5756b374d68cef2bebc47c1946c30491d8f4
Tish254 <tiegomseeraoscar295@gmail.com>
2023-05-31 00:11:22 +0300

    feat: Add Schedule; Added the scheduler to delete expired api tokens; branch: origin/main

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -43,6 +43,10 @@ class AuthController extends Controller
     }
 
     public function logout() {
-        return response()->json('This is my logout method');
+
+        Auth::user()->currentAccessToken()->delete();
+        return $this->success([
+            'message' => 'You have succesfully been logged out and your token has been removed'
+        ]);
     }
 }
